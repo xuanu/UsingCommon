ca735970d451a8902024ce3863ca665cda432bc2
zeffect <625417632@qq.com>
2018-01-25 16:51:15 +0800

    裁剪 准点

--- common/src/main/java/zeffect/cn/common/mp3/Mp3Utils.java
+++ common/src/main/java/zeffect/cn/common/mp3/Mp3Utils.java
@@ -129,16 +129,22 @@ public class Mp3Utils {
 
     private static final int SAMPLE_SIZE = 1024 * 200;
 
+
+    public static boolean clip(String inputPath, String outputPath, int start, int end) {
+        return clip(inputPath, outputPath, start, end, 0);
+    }
+
     /***
      * mp3 裁剪
      * @param inputPath
      * @param outputPath
      * @param start
      * @param end
+     * @param errorTime 误差时间，一般为一秒，播放器向下取整。默认0
      * @return
      */
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
-    public static boolean clip(String inputPath, String outputPath, int start, int end) {
+    public static boolean clip(String inputPath, String outputPath, int start, int end, long errorTime) {
         if (TextUtils.isEmpty(inputPath) || TextUtils.isEmpty(outputPath)) return false;
         if (start < 0 || end < start) return false;
         File inputFile = new File(inputPath);
@@ -171,7 +177,7 @@ public class Mp3Utils {
                 // >= 1000000是要裁剪停止和指定的裁剪结尾不小于1秒，否则可能产生需要9秒音频
                 //裁剪到只有8.6秒，大多数音乐播放器是向下取整，这样对于播放器变成了8秒，
                 // 所以要裁剪比9秒多一秒的边界
-                if (timeStamp > end) {//&& timeStamp - end >= 1000000
+                if (timeStamp > end && timeStamp - end >= errorTime) {
                     break;
                 }
                 if (sampleSize <= 0) {
